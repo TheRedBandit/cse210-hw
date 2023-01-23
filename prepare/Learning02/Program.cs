021e35cac5b62aa7adabcbf35f4c35bcf4ca3e23
Gideon Horrocks <gideonth@byui.edu>
2023-01-23 14:16:23 -0700

    More prep assignment changes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        /*Name: Allison Rose
+        Jobs:
+        Software Engineer (Microsoft) 2019-2022
+        Manager (Apple) 2022-2023*/
     }
 }
