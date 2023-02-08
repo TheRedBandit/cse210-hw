682f8b7b8c4e2bd675960cbcddfb335eea93c94e
Gideon Horrocks <gideonth@byui.edu>
2023-02-08 13:10:05 -0700

    Updated and Finished

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,8 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Fractions MyFractions = new Fractions();
-        Console.WriteLine(MyFractions.GetFractionString());
-        Console.WriteLine(MyFractions.GetDecimalValue());
+        Fractions Frac1 = new Fractions();
+        Console.WriteLine(Frac1.GetFractionString());
+        Console.WriteLine(Frac1.GetDecimalValue());
+
+        Fractions Frac2 = new Fractions(5);
+        Console.WriteLine(Frac2.GetFractionString());
+        Console.WriteLine(Frac2.GetDecimalValue());
+
+        Fractions Frac3 = new Fractions(3,4);
+        Console.WriteLine(Frac3.GetFractionString());
+        Console.WriteLine(Frac3.GetDecimalValue());
+
+        Fractions Frac4 = new Fractions(1,3);
+        Console.WriteLine(Frac4.GetFractionString());
+        Console.WriteLine(Frac4.GetDecimalValue());
     }
 }
