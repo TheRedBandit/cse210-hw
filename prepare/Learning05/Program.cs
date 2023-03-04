eb44c41b13348f53944af11b96f37c2f8b1e0e51
Gideon Horrocks <gideonth@byui.edu>
2023-03-04 12:20:30 -0700

    Finally Finished

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Console.Clear();
+
+        List<Shape> shapes = new List<Shape>();
+        
+        Square squ = new Square("Red",3);
+        Rectange rec = new Rectange("Blue", 4, 5);
+        Circle cir = new Circle("Green", 6);
+
+        shapes.Add(squ);
+        shapes.Add(rec);
+        shapes.Add(cir);
+
+        foreach (Shape i in shapes)
+        {
+            string color = i.GetColor();
+            double area = i.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+
     }
 }
---
+++ prepare/Learning05/Rectangle.cs
@@ -0,0 +1,16 @@
+public class Rectange : Shape
+{
+    private double _length;
+    private double _width;
+
+    public Rectange(string color, double length, double width) : base (color)
+    {
+        _length = length;
+        _width = width;
+    }
+
+    public override double GetArea()
+    {
+        return _length * _width;
+    }
+}
